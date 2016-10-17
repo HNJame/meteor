21df807ba9c342433fc10389515df40c4a148d15
ant2nebula@gmail.com <ant2nebula@gmail.com>
2016-10-17 10:04:00 +0800

    json内嵌json -> MapType

--- server/src/main/scala/com/meteor/server/executor/ImportKafkaTaskExecutor.scala
+++ server/src/main/scala/com/meteor/server/executor/ImportKafkaTaskExecutor.scala
@@ -1,11 +1,19 @@
 package com.meteor.server.executor
 
+import scala.collection.mutable.ListBuffer
+
+import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.annotation.Experimental
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.MapType
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.StructField
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.storage.StorageLevel
 
 import com.meteor.model.view.importqueue.ImportKafkaTask
 import com.meteor.server.context.ExecutorContext
 import com.meteor.server.executor.instance.InstanceTaskExecutor
-import org.apache.spark.storage.StorageLevel
 
 class ImportKafkaTaskExecutor extends AbstractTaskExecutor {
 
@@ -13,15 +21,26 @@ class ImportKafkaTaskExecutor extends AbstractTaskExecutor {
     val task = instanceTaskExecutor.instanceTask.getTask.asInstanceOf[ImportKafkaTask]
     val rdd = paramMap.get("rdd").get.asInstanceOf[RDD[String]]
     rdd.persist(StorageLevel.MEMORY_ONLY)
-    
+
     val one = rdd.first()
     val oneRDD = ExecutorContext.hiveContext.sparkContext.parallelize(Seq(one), 1)
     val oneSchema = ExecutorContext.hiveContext.read.json(oneRDD).schema
 
+    val list = ListBuffer[StructField]()
+    for (sf <- oneSchema.iterator) {
+      if (sf.dataType.isInstanceOf[StructType]) {
+        list += StructField(sf.name, MapType(StringType, StringType, true))
+      } else {
+        list += sf
+      }
+    }
+    val mapTypeOneSchema = StructType(list.seq)
+
     val reader = ExecutorContext.hiveContext.read
-    reader.schema(oneSchema)
+    reader.schema(mapTypeOneSchema)
     reader.json(rdd).registerTempTable(task.getRegTable)
     ExecutorContext.hiveContext.sql(s"CACHE TABLE ${task.getRegTable}")
+
     rdd.unpersist(false)
   }
 
